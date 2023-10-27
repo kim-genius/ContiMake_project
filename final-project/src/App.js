06d848e50dd2ac2f8f35fe9440de2dc72305ac06
SSJ <sonsj33@gmail.com>
2023-10-27 20:18:52 +0900

    generate page layout

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,12 +2,14 @@ import './features/styles/global.css'
 import Header from './features/ui/header/Header';
 import { Route,Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Generatepage from './pages/Generatepage';
 function App() {
   return (
     <div>
-      <Header></Header>
+      {/* <Header></Header> */}
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/generate' element={<Generatepage/>}></Route>
       </Routes>
     </div>
   );
--- final-project/src/pages/Generatepage.jsx
+++ final-project/src/pages/Generatepage.jsx
@@ -1,10 +1,14 @@
-
 import React from 'react'
-
+import styles from './Generatepage.module.css'
 const Generatepage = () => {
   return (
-    <div>
-      
+    <div className={styles.generatepageWrapper}>
+      <nav className={styles.navBar}></nav>
+      <div className={styles.contentsWrapper}>
+        <section className={styles.designTab}></section>
+        <section className={styles.canvas}></section>
+        <section className={styles.designTab}></section>
+      </div>
     </div>
   )
 }
