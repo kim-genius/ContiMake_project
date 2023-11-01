498cce6ce609ac32fda76dadaf37d693bdc4d7b5
kim young jae <jae2942@gmail.com>
2023-11-01 16:32:48 +0900

    home add

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -2,6 +2,7 @@ import './features/styles/global.css'
 import Header from './features/ui/header/Header';
 import { Route,Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './features/ui/footer/Footer';
 function App() {
   return (
     <div>
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
+      <Footer></Footer>
+      
     </div>
   );
 }
