6bef2ebf2ba3432bc57ce5573cb0e904992087e4
sonsujin <sonsj33@gmail.com>
2023-11-02 17:11:01 +0900

    add react-sketch-canvas library, and edit page layout.

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -3,13 +3,16 @@ import Header from './features/ui/header/Header';
 import { Route,Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Generatepage from './pages/Generatepage';
+import Inpainting from './features/inpainting/Inpainting';
+import Editpage from './pages/Editpage';
 function App() {
   return (
     <div>
-      {/* <Header></Header> */}
+      <Header></Header> 
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/generate' element={<Generatepage/>}></Route>
+        <Route path='/edit' element={<Editpage/>}></Route>
       </Routes>
     </div>
   );
