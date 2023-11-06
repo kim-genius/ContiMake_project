b1738b57446925c24a229d042b9bb08d5f593567
ldy <egni1227@naver.com>
2023-11-06 15:34:16 +0900

    google login

--- config/database.js
+++ config/database.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '10040415',
+    password: '1234',
     port: 3306,
-    database: 'consoup'
+    database: 'nodejs'
 });
 conn.connect();
 
