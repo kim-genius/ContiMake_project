f7e1d8404a51a15daffd48736f08f544d9d88107
sonsujin <sonsj33@gmail.com>
2023-11-07 10:25:35 +0900

    생성 페이지 헤더 작업

--- final-project/src/pages/Generatepage.jsx
+++ final-project/src/pages/Generatepage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import styles from './Generatepage.module.scss'
 import Inpainting from '../features/inpainting/Inpainting'
-
+import HeaderNav from '../features/generate/components/HeaderNav'
+import ToggleBtn from '../features/ui/toggleBtn/ToggleBtn'
 const Generatepage = () => {
   return (
     <div className={styles.Wrapper}>
       <nav className={styles.navBar}>
-        <span>제목없음</span>
-        <button>EditIcon</button>
-        <button>마이콘티 저장버튼</button>
-        <button>내보내기 버튼</button>
+        <HeaderNav />
       </nav>
       <div className={styles.contentsWrapper}>
-        <section className={styles.designTab}></section>
+        <section className={styles.designTab}>
+          <ToggleBtn tab1={"생성"} tab2={"편집"} />
+        </section>
         <section className={styles.canvas}>
         </section>
         <section className={styles.designTab}></section>
