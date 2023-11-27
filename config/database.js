d00c148738a86af9e5c1c663ca3704cdaf533c33
LBD <ehdclrltk12@naver.com>
2023-11-27 11:18:56 +0900

    FEAT : DB에 프로젝트 저장 추가

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 // 내가 연결할 DB에 대한 정보ss
 const mysql = require('mysql2');
 const conn = mysql.createConnection({
-    host: 'project-db-stu3.smhrd.com',
-    user: 'Insa4_JSB_final_2',
-    password: 'aishcool2',
-    port: 3307,
-    database: 'Insa4_JSB_final_2'
+  host: "project-db-stu3.smhrd.com",
+  user: "Insa4_JSB_final_2",
+  password: "aishcool2",
+  port: 3307,
+  database: "Insa4_JSB_final_2",
+  connectionLimit: 10
 });
 conn.connect();
 
