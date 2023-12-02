6d39224a1a0f4d792f231746d9d4bb333c13d695
sonsujin <sonsj33@gmail.com>
2023-12-02 14:53:48 +0900

    CHORE : 인페인팅 모델링 작업, 편집 페이지 로딩 바 교체

--- modelAPI/api.py
+++ modelAPI/api.py
@@ -8,6 +8,9 @@ from io import BytesIO
 import base64
 import PIL
 import numpy as np
+
+from typing import Optional
+from pydantic import BaseModel
 app = FastAPI()
 
 app.add_middleware(
@@ -41,52 +44,45 @@ pipe2 = StableDiffusionInpaintPipeline.from_pretrained(
     "runwayml/stable-diffusion-inpainting", torch_dtype=torch.float16
 ).to(device)
 
-# inpainting 기능
-# @app.get('/inpainting')
-# def inpainting(prompt, mask_data, image_data):
-
-#     # "data:image/png;base64," 부분을 제외한 Base64 인코딩된 문자열 추출
-#     mask_img_base64_string = mask_data.split(",")[1]
-#     init_image = PIL.Image.open(BytesIO(base64.b64decode(image_data))).convert("RGB")
-
-
-#     open_mask_image = PIL.Image.open(BytesIO(base64.b64decode(mask_img_base64_string))).convert("RGB")
-
-#     mask_image = PIL.ImageOps.invert(open_mask_image)
-#     open_mask_image.show()
-#     mask_image.save('test_mask.png')
-
-#     with autocast(device):
-#         inpainting_image = pipe(prompt = prompt, image = init_image, mask_image = open_mask_image).images[0]
-#         inpainting_image.save("test_inpainting.png")
-#         buffer = BytesIO()
-#         inpainting_image.save(buffer, format="PNG")
-#         imgstr = base64.b64encode(buffer.getvalue())
-#         result = imgstr.decode('ascii')
-
-#     return Response(content=result, media_type="text/plain")
+class Item(BaseModel):
+    edited_prompt : str
+    mask_data : str
+    image_data : str
 
 # inpainting 기능
-@app.get('/inpainting')
-def inpainting(prompt: str, mask_data: str, image_data: str):
+@app.post('/inpainting')
+def inpainting(item: Item):
 
-    init_image = PIL.Image.open(BytesIO(base64.b64decode(image_data))).convert("RGB")
-    print(init_image)
+    
+    init_image = PIL.Image.open(BytesIO(base64.b64decode(item.image_data))).resize((512,512))
+    
+    try :
+        decode_mask = base64.b64decode(item.mask_data)
+        bytes_mask = BytesIO(decode_mask)
+        open_mask_image = PIL.Image.open(bytes_mask).resize((512,512))
+        rgb_image = PIL.Image.new("RGB", open_mask_image.size, (255,255,255))
+        rgb_image.paste(open_mask_image, (0, 0), open_mask_image)
+        rgb_image.save('rgb_image.png')
+        print('이미지가 성공적으로 저장됨')
+    except Exception as e:
+        print(f"Base64 decode Error: {e}")
 
-    open_mask_image = PIL.Image.open(BytesIO(base64.b64decode(mask_data))).convert("RGB")
-
-    mask_image = PIL.ImageOps.invert(open_mask_image)
-    open_mask_image.show()
+    mask_image = PIL.ImageOps.invert(rgb_image)
     mask_image.save('test_mask.png')
-
+    init_arr = np.array(init_image)
+    mask_arr = np.array(rgb_image)
+    print("init",init_arr.shape)
+    print("mask_image",mask_arr.shape)
     with autocast(device):
-        inpainting_image = pipe2(prompt = prompt, image = init_image, mask_image = mask_image).images[0]
+        inpainting_image = pipe2(prompt = item.edited_prompt, image = init_image, mask_image = mask_image).images[0]
         inpainting_image.save("test_inpainting.png")
-        buffer = BytesIO()
+        buffer = BytesIO() 
         inpainting_image.save(buffer, format="PNG")
         imgstr = base64.b64encode(buffer.getvalue())
         result = imgstr.decode('ascii')
 
     return Response(content=result, media_type="text/plain")
+
+
     
 
